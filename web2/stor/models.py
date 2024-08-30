a8104966e77ecfb741522140408170c0e0c2b294
MatheusPMatos <mpeima@yahoo.com.br>
2024-08-30 19:23:08 -0300

    comit parcial models

--- web2/stor/models.py
+++ web2/stor/models.py
@@ -9,4 +9,28 @@ class User(models.Model):
     userType = models.IntegerField()
     
     def __str__(self) -> str:
-        return super().__str__()
+        return self.name
+    
+class Products(models.Model):
+    name = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+    
+    
+    
+class ProductSchuedule(models.Model):
+    
+    WeekDay = models.IntegerField()
+    InitialTime = models.TimeField()
+    FinalTime = models.TimeField()
+    Product = models.ForeignKey(Products)    
+
+    
+class Rent(models.Model):
+    
+    RentTime = models.TimeField()
+    User = models.ForeignKey(User)
+    ProductSchuedule = models.ForeignKey(ProductSchuedule)
+    
+
+    
+    
