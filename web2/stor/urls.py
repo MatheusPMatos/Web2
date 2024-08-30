d2b8321fffc6026ca298837779f316894adaa11b
MatheusPMatos <mpeima@yahoo.com.br>
2024-08-30 20:54:45 -0300

    cimit

--- web2/stor/urls.py
+++ web2/stor/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UserView
 
 urlpatterns = [
-    path('<int:user_id>/', UserView.as_view()),
-    #path('user/<int:user_id>/', ProfessorReadUpdateDeleteView.as_view(), name='professor-detail'),
+    path('/', UserView.as_view()),
+    #path('<int:user_id>/', ProfessorReadUpdateDeleteView.as_view(), name='professor-detail'),
 ]
