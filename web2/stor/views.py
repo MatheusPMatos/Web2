919bad30b8339cf832405b5704bb425595886ed4
Fabio Morais <104800091+FabioMoraiss@users.noreply.github.com>
2024-08-30 22:47:19 +0000

    comit view User get and response

--- web2/stor/views.py
+++ web2/stor/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserView():
+    def get(self, request):
+        users = User.objects.all()
+        serializer = Userserializer(users, many=true)
+        return Response(serializer.data, satus=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = userSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,  satus=status.HTTP_200_OK)
+
+
+
+def lista_usuarios(request):
+    usuarios = User.objects.all()
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': User})
+
+
